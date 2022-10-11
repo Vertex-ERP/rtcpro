0cd9811b95b2a15dc71d9f2380b53fa651811660
osama1998H <osamamuhammed555@gmail.com>
2022-10-11 15:19:41 +0300

    add instructor applicant

---
+++ rtcpro/rtcpro/doctype/instructor_applicant/instructor_applicant.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, osama and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Instructor Applicant', {
+	// refresh: function(frm) {
+
+	// }
+});
