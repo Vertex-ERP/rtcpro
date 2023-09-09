d464491f8e728217ce018007fea18e59ae800d62
osama1998H <osamamuhammed555@gmail.com>
2023-09-09 10:34:09 +0300

    delete doctypes

--- rtcpro/rtcpro/doctype/advantages/advantages.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, osama and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class Advantages(Document):
-	pass
--- rtcpro/rtcpro/doctype/pricing_table/pricing_table.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, osama and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Pricing Table', {
-	// refresh: function(frm) {
-
-	// }
-});
